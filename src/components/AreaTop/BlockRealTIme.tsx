e5969272d7d561c5827632b6e746eea69102cbea
Kenta Urakami <ken.atnek@gmail.com>
2025-09-20 15:23:29 +0900

    エリアTOP 該当部NG画像

--- src/components/AreaTop/BlockRealTIme.tsx
+++ src/components/AreaTop/BlockRealTIme.tsx
@@ -100,7 +100,15 @@ const BlockRealTIme = () => {
                 <span className={styles.gradeLabel}>
                   {gradeMap[cast.gradeId]?.label}
                 </span>
-                <Image src={cast.castImage} alt={cast.castName} fill />
+                <Image
+                  src={
+                    cast.castImage && cast.castImage !== ''
+                      ? cast.castImage
+                      : `/images/cast/${cast.shopId}/no-image.webp`
+                  }
+                  alt={cast.castName}
+                  fill
+                />
               </div>
             </Link>
             <div className={styles.castProfile}>
