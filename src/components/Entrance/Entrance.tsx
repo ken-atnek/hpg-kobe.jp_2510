268ced6e35837ba691a7f62fd2300ad7f9141718
Kenta Urakami <ken.atnek@gmail.com>
2025-12-17 12:50:34 +0900

    神戸VILLA相互リンク・メニュー追加

--- src/components/Entrance/Entrance.tsx
+++ src/components/Entrance/Entrance.tsx
@@ -154,18 +154,37 @@ export default function Entrance({
                 loading="lazy" // 遅延読み込み
               />
             </a>
+
             <a
               href="https://www.girlsheaven-job.net/hyogo/ma-45/sa-460/koube_hp_part3/?of=y"
               target="_blank"
-              rel="noopener noreferrer" // セキュリティ対策
+              rel="noopener noreferrer"
             >
               <Image
                 src="https://img.cityheaven.net/img/kikaku/hy_memberdiary/wp-content/uploads/2023/09/GH遷移バナー_オフィシャル用-1.png"
-                alt="ガールズヘブン求人サイトへ" // alt属性追加
-                width={600} // 適切なサイズ指定
-                height={100} // 適切なサイズ指定
-                style={{ width: '100%', height: 'auto' }} // レスポンシブ対応
-                loading="lazy" // 遅延読み込み
+                alt="ガールズヘブン求人サイトへ"
+                width={600}
+                height={100}
+                style={{ width: '100%', height: 'auto' }}
+                loading="lazy"
+              />
+            </a>
+            <a
+              href="https://yoasobi-heaven.com/en/hyogo"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="https://yoasobi-heaven.com/img/renewal/linklist/bn_yoasobi-heaven_200x40.jpg"
+                alt="ガールズヘブン求人サイトへ"
+                width={200}
+                height={400}
+                style={{
+                  width: '200px',
+                  height: 'auto',
+                  alignSelf: 'center',
+                }}
+                loading="lazy"
               />
             </a>
           </div>
--- src/data/villa/navMenuData.ts
+++ src/data/villa/navMenuData.ts
@@ -46,6 +46,13 @@ export const navMenu = [
     target: true,
   },
   {
+    id: 'navEvent',
+    href: 'https://www.cityheaven.net/hyogo/A2802/A280201/koube_hp_part3/reviews/?of=y',
+    label: '口コミ',
+    labelEn: 'reviews',
+    target: true,
+  },
+  {
     id: 'navSystem',
     href: '/villa/system/',
     label: 'システム',
